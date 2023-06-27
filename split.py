9d34300a6982eb6a1b6baeb335eb48eecf831c6f
David Sackstein <davids@codeprecise.com>
2023-06-27 04:01:14 +0300

    Complete

--- split.py
+++ split.py
@@ -1,11 +1,12 @@
 import struct
 
 
-# Define the data for each block
+# Define the data for each block as (value, size)
 blocks = [
-    (1, 10),  # Block 1: All 1's of size 100
-    (2, 11),  # Block 2: All 2's of size 200
-    (3, 45)  # Block 3: All 3's of size 300
+    (1, 10),
+    (2, 10),
+    (3, 10),
+    (4, 10)
 ]
 
 
@@ -19,38 +20,84 @@ def create_file(file_name):
             file.write(struct.pack('B', value) * size)
 
 
-def split_file(file_name, max_size):
+# A chunk is a piece of the original file that
+# - contains whole lines (a line is 2 bytes of size followed by size bytes of encrypted data).
+# - is not larger than the max_chunk_size
+# ChunkWriter writes chunks, switching to a new chunk whenever appending a new line would exceed the max_chunk_size.
+class ChunkWriter:
+    def __init__(self, base_name, max_chunk_size):
+        self.index = 0
+        self.base_name = base_name
+        self.max_chunk_size = max_chunk_size
+        self.chunk_file = None
+        self.acc_chunk_size = 0
+        self.new_chunk()
 
-    acc_data = []
-    index = 0
-    with open(file_name, "rb") as file:
-        while True:
-            data = file.read(2)
+    def new_chunk(self):
+        # close the chunk file, (if not None), before creating a new one
+        self.close()
 
-            if not data:  # Reached EOF
-                break
+        # increase the index and reset the accumulated size to 0
+        self.index = self.index + 1
+        self.acc_chunk_size = 0
 
-            # Unpack the 2-byte integer using network byte order (big-endian)
-            size = struct.unpack('!H', data)[0]
+        # open a new chunk file
+        chunk_file_name = f"{self.base_name}.{self.index:02d}.bin"
+        self.chunk_file = open(chunk_file_name, "wb")
 
-            acc_data += file.read(size)
+    def write_line(self, line_bytes, line_size):
+        # If it is too large for the current chunk, tell the writer to close
+        # the current chunk and open a new one.
+        if self.acc_chunk_size + line_size > self.max_chunk_size:
+            self.new_chunk()
 
-            if len(acc_data) > max_size:
-                # Write acc_data to file with index
-                partial_name = file_name + f'index'
-                # Write the data to the file named partial_name
+        # Write the line as the two bytes line_size followed by the line bytes.
+        self.chunk_file.write(struct.pack('!H', line_size))
+        self.chunk_file.write(line_bytes)
 
-                # Increment the index and zero acc_data
-                index += 1
-                acc_data = []
-                pass
+        # Accumulate the size
+        self.acc_chunk_size += line_size
 
-            # Advance the position by the value of the 2 bytes read
-            file.seek(size, 1)
+    def close(self):
+        if self.chunk_file:
+            self.chunk_file.close()
+
+
+def split_file(file_name, max_chunk_size):
+
+    # Open the original_file
+    with open(file_name, "rb") as original_file:
+
+        writer = ChunkWriter(file_name, max_chunk_size)
+
+        write_chunks(original_file, writer)
+
+        writer.close()
+
+
+def write_chunks(original_file, writer):
+
+    while True:
+
+        # Read the size of the next line
+        size_bytes = original_file.read(2)
+        if not size_bytes:
+            break
+
+        line_size = struct.unpack('!H', size_bytes)[0]
+
+        # Now read the line
+        line_bytes = original_file.read(line_size)
+        if not line_bytes:
+            # This is actually an error because line_size is expected to be greater than zero
+            break
+
+        # Write it to the chunk
+        writer.write_line(line_bytes, line_size)
 
 
 filename = "binary_file"
 
 create_file(filename)
 
-split_file(filename, 12)
+split_file(filename, 20)
